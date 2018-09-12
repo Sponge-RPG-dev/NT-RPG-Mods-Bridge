cc0b84805911e7dc35d6ebc97caa1bd68c5e9d7c
GlassSpirit <yurikochkun@gmail.com>
2018-09-12 22:38:18 +0300

    Fixing vanilla fallback

--- src/main/java/ru/glassspirit/customnpcsbridge/CustomNPCsBridge.java
+++ src/main/java/ru/glassspirit/customnpcsbridge/CustomNPCsBridge.java
@@ -1,19 +1,8 @@
 package ru.glassspirit.customnpcsbridge;
 
 import com.google.inject.Inject;
-import cz.neumimto.core.ioc.IoC;
-import cz.neumimto.rpg.Log;
-import cz.neumimto.rpg.configuration.DebugLevel;
-import cz.neumimto.rpg.players.CharacterService;
-import cz.neumimto.rpg.players.ExperienceSources;
-import cz.neumimto.rpg.players.IActiveCharacter;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
-import noppes.npcs.api.NpcAPI;
-import noppes.npcs.api.constants.EntityType;
-import noppes.npcs.api.event.NpcEvent;
-import noppes.npcs.api.event.QuestEvent;
 import org.slf4j.Logger;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
@@ -22,8 +11,6 @@ import org.spongepowered.api.event.game.state.GameAboutToStartServerEvent;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
-
-import java.util.UUID;
 
 @Plugin(
         id = "nt-rpg-customnpcs-bridge",
@@ -50,8 +37,6 @@ public class CustomNPCsBridge {
     @DefaultConfig(sharedRoot = true)
     private ConfigurationLoader<CommentedConfigurationNode> config;
 
-    private CharacterService characterService;
-
     @Listener
     public void onGameInitialization(GameInitializationEvent event) {
         loadConfig();
@@ -66,42 +51,10 @@ public class CustomNPCsBridge {
     public void onGameAboutToStartServer(GameAboutToStartServerEvent event) {
         try {
             Class.forName("noppes.npcs.api.NpcAPI");
-            if (NpcAPI.IsAvailable()) {
-                NpcAPI.Instance().events().register(this);
-                characterService = IoC.get().build(CharacterService.class);
-                logger.info("Registering CustomNPCs event listener");
-            }
+            new CustomNPCsEventListener().setup();
+            logger.info("CustomNPCs event listener registered!");
         } catch (ClassNotFoundException e) {
             logger.error("CustomNPCs not found!", e);
-        }
-    }
-
-    @SubscribeEvent
-    public void onNpcQuestCompletion(QuestEvent.QuestTurnedInEvent event) {
-        IActiveCharacter character = characterService.getCharacter(UUID.fromString(event.player.getUUID()));
-        if (character != null) {
-            characterService.addExperiences(character, event.expReward, ExperienceSources.QUESTING);
-            Log.info(String.format("Adding %s experience to %s for completing quest \"%s\"",
-                    event.expReward,
-                    character.getPlayer().getName(),
-                    event.quest.getName()),
-                    DebugLevel.BALANCE);
-        }
-    }
-
-    @SubscribeEvent
-    public void onNpcDeath(NpcEvent.DiedEvent event) {
-        if (event.damageSource.getTrueSource().typeOf(EntityType.PLAYER)) {
-            IActiveCharacter character = characterService.getCharacter(UUID.fromString(event.damageSource.getTrueSource().getUUID()));
-            if (character != null) {
-                int experience = event.npc.getInventory().getExpRNG();
-                characterService.addExperiences(character, experience, ExperienceSources.PVE);
-                Log.info(String.format("Adding %s experience to %s for killing npc \"%s\"",
-                        experience,
-                        character.getPlayer().getName(),
-                        event.npc.getName()),
-                        DebugLevel.BALANCE);
-            }
         }
     }
 
